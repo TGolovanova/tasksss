08a388f689ec76126031bcc91e1dbd67ac8b33bb
TGolovanova <ferrana2430@yandex.ru>
2021-12-08 12:03:20 +0300

    добавлен Дата Провайдер

--- src/test/java/TrainTest.java
+++ src/test/java/TrainTest.java
@@ -1,27 +1,25 @@
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
 public class TrainTest {
 
-
-
-    @Test
-    public static void deleteOrNot() {
-
-        int[] array = new int[]{1, 4, 10, 10, 4, 9, 4};
-        int someInt = 4;
-
-        Assert.assertEquals(new trainTask().deleteInt(array, someInt), Arrays.toString(new int[]{1,10,10,9}));
+    @DataProvider
+    public Object[][] forTests() {
+        return new Object[][]{
+                {new int[]{1, 4, 10, 10, 4, 9, 4}, 4, new int[]{1,10,10,9}},
+                {new int[]{1, 4, 10, 10, 4, 9, 4}, 10, new int[]{1,4,4,9,4}},
+                {new int[]{1, 4, 1, 1, 44, 99, 24}, 1, new int[]{4,44,99,24}},
+                {new int[]{14, 47, 105, 170, 47, 94, 400}, 47, new int[]{14,105,170,94, 400}}
+        };
     }
 
-    @Test
-    public static void noDelete() {
+    @Test(dataProvider = "forTests")
+    public static void deleteOrNot(int[] one, int two, int[] three) {
 
-        int[] array = new int[]{1, 4, 10, 10, 4, 9, 4};
-        int someInt = 10;
-
-        Assert.assertNotEquals(new trainTask().deleteInt(array, someInt), Arrays.toString(new int[]{1, 4, 10, 4, 9, 4}));
+        Assert.assertEquals(new trainTask().deleteInt(one, two), Arrays.toString(three));
     }
+
 }
